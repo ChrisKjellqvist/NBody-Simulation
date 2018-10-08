c06f23133a7ac0b19e1250789c6ae08dab5ec62d
Christopher Kjellqvist <chriskjellqvist@gmail.com>
2018-10-08 12:06:57 -0400

    more comments. Moved definitions to constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -1,7 +1,28 @@
 #ifndef CONSTANTS_NBODY_HPP
 #define CONSTANTS_NBODY_HPP
 
-int NOFB_INIT = 512;
-//int ITERATIONS = 1000;
+// number of initial bodies
+#define NOFB_INIT 512
+// number of iterations (comment/uncomment to enable/disable)
+#define ITERATIONS 600
+// define time_step if we're not doing a variable time_step
+#ifndef VARIABLE_TS
+#define TIME_STEP 1.0
+#endif
+// gravitational constant
+#define GRAV_CONST (double)6.67408e-11
+// TODO: replace with CUDA rsqrt. Inverse sqrt
+#ifndef rsqrt
+#define rsqrt 1.0/sqrt
+#endif
+
+// 2^NOFB_BIT_SIZE options of bodies per bit
+#define NOFB_BIT_SIZE 4
+
+// make sure that number of initial bodies > 2
+#if NOFB_INIT < 2
+#error "NOFB_INIT must be defined as 2 or more"
+#endif
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,24 +6,6 @@
 #include <math.h>
 #include "SolarSystem.hpp"
 #include "constants.hpp"
-#ifndef VARIABLE_TS
-#define TIME_STEP 1.0
-#endif
-#define GRAV_CONST (double)6.67408e-11
-#ifndef NOFB_INIT
-#define NOFB_INIT 512
-#endif
-#ifndef rsqrt
-#define rsqrt 1.0/sqrt
-#endif
-// 2^NOFB_BIT_SIZE options of bodies per bit
-#define NOFB_BIT_SIZE 4
-
-#if NOFB_INIT < 2
-#error "NOFB_INIT must be defined as 2 or more"
-#endif
-
-
 
 int nofB = NOFB_INIT;
 double pX[NOFB_INIT];
@@ -45,13 +27,14 @@ void handle_sigs(int signo){
 #pragma acc routine seq
 extern double sqrt(double f);
 int main() {
-  srand(time(0));
+  srand(509);
 
   // OpenACC doesn't like while(1), so we have this monstrosity instead
   struct sigaction sa;
   sa.sa_handler = handle_sigs;
   sigaction(SIGINT, &sa, NULL);
 
+  //initialize vectors
   for (int i = 0; i < nofB; i++) {
     Body t;
     vX[i] = t.velocityX;
@@ -79,10 +62,9 @@ int main() {
 #endif
 
 // only need p{X,Y,Z} if we're sure there's a collision.
-#pragma acc data copyout(pX, pY, pZ)
     for(int i = 0; i < 100; ++i){
       int curr;
-#pragma acc kernels private(curr) async(2)
+#pragma acc kernels private(curr) async(2) wait(1)
       for(int my_planet = 0; my_planet < nofB; ++my_planet){
 	double deltaX =0, deltaY =0, deltaZ =0;
 	int collided_with = 0;
@@ -113,16 +95,15 @@ int main() {
 	  // also enables us to do more than one collision
 	  collided_with |= (recip_dist > .000000001) << (curr >> NOFB_BIT_SIZE);
 
-	  //collided_with |= (recip_dist > .000000001);
 	  double ds = recip_dist*recip_dist*recip_dist * masses[curr] * GRAV_CONST;
 	  // a = m1 * G / d^2
-	  deltaX -= dX * ds;
-	  deltaY -= dY * ds;
-	  deltaZ -= dZ * ds;
+	  deltaX += dX * ds;
+	  deltaY += dY * ds;
+	  deltaZ += dZ * ds;
 	}
-	vX[my_planet] += deltaX * TIME_STEP;
-	vY[my_planet] += deltaY * TIME_STEP;
-	vZ[my_planet] += deltaZ * TIME_STEP;
+	vX[my_planet] -= deltaX * TIME_STEP;
+	vY[my_planet] -= deltaY * TIME_STEP;
+	vZ[my_planet] -= deltaZ * TIME_STEP;
 	collisions[my_planet] = collided_with;
       }
 #pragma acc kernels async(1) wait(2)
@@ -132,13 +113,26 @@ int main() {
 	pZ[curr] += vZ[curr] * TIME_STEP;
       }
     }
-#pragma acc update host(collisions)
+
+    //handle collisions
+#pragma acc update host(collisions[0:nofB])
+    int has_updated = 0;
+    int bot = nofB;
     for (int i = 0; i < nofB; ++i){
       while (collisions[i] != 0){
-#pragma acc update host(vX, vY, vZ, pX, pY, pZ)
+	if (!has_updated){
+#pragma acc update host(vX[0:nofB],\
+    vY[0:nofB],\
+    vZ[0:nofB],\
+    pX[0:nofB],\
+    pY[0:nofB],\
+    pZ[0:nofB])
+	  has_updated = 1;
+	}
 	int p1 = i;
 	int p2;
 	double minLength = 2e100;
+	// find which body i collided with
 	for (int j = 0; j < nofB; ++j){
 	  if ((j >> NOFB_BIT_SIZE) & collisions[i] == 0)
 	    continue;
@@ -157,6 +151,8 @@ int main() {
 	vY[p1] = (vY[p1]*masses[p1]+vY[p2]*masses[p2])/newmass;
 	vZ[p1] = (vZ[p1]*masses[p1]+vZ[p2]*masses[p2])/newmass;
 	masses[p1] = newmass;
+	// shift everything and decrement nofB so we
+	// never look at it anymore
 	for(int j = p2; j < nofB-1; ++j){
 	  pX[j] = pX[j+1];
 	  pY[j] = pY[j+1];
@@ -167,13 +163,21 @@ int main() {
 	  masses[j] = pX[j+1];
 	}
 	collisions[i] = 0;
+	if (p1 < bot)
+	  bot = p1;
 	--nofB;
       }
-#pragma acc wait(1)
-#pragma acc update device(nofB, pX, pY, pZ, vX, vY, vZ, masses)
     }
-#pragma acc wait(1)
+    if (has_updated){
+#pragma acc update device(nofB, \
+    pX[bot:nofB],\
+    pY[bot:nofB],\
+    pZ[bot:nofB],\
+    vX[bot:nofB],\
+    vY[bot:nofB],\
+    vZ[bot:nofB],\
+    masses[bot:nofB])
+    }
   }
-#pragma acc update host(pX)
   std::cout << nofB<< "\n";
 }
