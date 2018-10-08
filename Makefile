828ca2c8a513f683a285e68ccb80e4f68cb1dac1
Christopher Kjellqvist <chriskjellqvist@gmail.com>
2018-10-08 08:15:30 -0400

    Works more now I think

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 # other compilers work but this is the once that I've used before
 CC=pgc++
+#CC=clang++-5.0
 # uncomment the stuff on the next line to have a limited number of iterations
 # instead of an infinite loop
-OPTS=-Minfo=accel -O3 #-DITERATIONS=100
+OPTS= -Minfo=accel -O3 -DITERATIONS=200
 SRC=main.cpp SolarSystem.cpp
 
 all: 
-	@$(CC) $(OPTS) -acc $(SRC) -o ss_gpu
+	@$(MAKE) gpu
 serial:
 	@$(CC) $(OPTS) $(SRC) -o ss_serial
+var:
+	@$(CC) $(OPTS) $(SRC) -DVARIABLE_TS -o ss_serial
+gpu:
+	@$(CC) $(OPTS) -acc $(SRC) -o ss_gpu
 clean:
 	@rm -f ss_gpu ss_serial *.o
 	@echo "Cleaning..."
--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <signal.h>
 #include <ctime>
 #include <stdlib.h>
+#include <iostream>
 #include <math.h>
 #include "SolarSystem.hpp"
-#define TIME_STEP 1
-#define GRAV_CONST (float)6.67408e-11
+#ifndef VARIABLE_TS
+#define TIME_STEP 1.0
+#endif
+#define GRAV_CONST (double)6.67408e-11
 #ifndef NOFB_INIT
 #define NOFB_INIT 512
 #endif
 #ifndef rsqrt
-#define rsqrt 1.0/sqrtf
+#define rsqrt 1.0/sqrt
 #endif
 // 2^NOFB_BIT_SIZE options of bodies per bit
 #define NOFB_BIT_SIZE 4
@@ -19,103 +22,129 @@
 #error "NOFB_INIT must be defined as 2 or more"
 #endif
 
-int nofB = NOFB_INIT;
-float pX[NOFB_INIT];
-float pY[NOFB_INIT];
-float pZ[NOFB_INIT];
-float vX[NOFB_INIT];
-float vY[NOFB_INIT];
-float vZ[NOFB_INIT];
-float masses[NOFB_INIT];
-int collisions[NOFB_INIT];
 
+int nofB = NOFB_INIT;
+double pX[NOFB_INIT];
+double pY[NOFB_INIT];
+double pZ[NOFB_INIT];
+double vX[NOFB_INIT];
+double vY[NOFB_INIT];
+double vZ[NOFB_INIT];
+double masses[NOFB_INIT];
+int collisions[NOFB_INIT];
+#ifdef VARIABLE_TS
+double TIME_STEP;
+#endif
 int sig = 0;
 void handle_sigs(int signo){
   sig = signo == SIGINT;
 }
+//
 #pragma acc routine seq
-extern float sqrtf(float f);
+extern double sqrt(double f);
 int main() {
   srand(time(0));
+
+  // OpenACC doesn't like while(1), so we have this monstrosity instead
+  struct sigaction sa;
+  sa.sa_handler = handle_sigs;
+  sigaction(SIGINT, &sa, NULL);
 
   for (int i = 0; i < nofB; i++) {
     Body t;
     vX[i] = t.velocityX;
     vY[i] = t.velocityY;
     vZ[i] = t.velocityZ;
+    pX[i] = t.positionX;
+    pY[i] = t.positionY;
+    pZ[i] = t.positionZ;
     masses[i] = t.mass;
+    collisions[i] = 0;
   }
-  collisions[0] = collisions[1] = 0;
+#ifdef ITERATIONS
   int cnt = 0;
+#endif
 #pragma acc data copyin(pX, pY, pZ, vX, vY, vZ, masses, collisions, nofB)
   while(
 #ifdef ITERATIONS 
       cnt++ < ITERATIONS &&
 #endif
-    !sig){
+      !sig){
 #ifdef ITERATIONS
     if (cnt % 1000 == 0){
       printf("%d\n", cnt);
     }
 #endif
 
+// only need p{X,Y,Z} if we're sure there's a collision.
 #pragma acc data copyout(pX, pY, pZ)
     for(int i = 0; i < 100; ++i){
       int curr;
 #pragma acc kernels private(curr) async(2)
       for(int my_planet = 0; my_planet < nofB; ++my_planet){
-	float deltaX =0, deltaY =0, deltaZ =0;
+	double deltaX =0, deltaY =0, deltaZ =0;
 	int collided_with = 0;
-	for(curr = 0; curr < my_planet; ++curr){
-	  float dX = pX[my_planet]-pX[curr];
-	  float dY = pY[my_planet]-pY[curr];
-	  float dZ = pZ[my_planet]-pZ[curr];
+	for(curr = 0; curr < nofB; ++curr){
+	  if (curr == my_planet)
+	    continue;
+	  double dX = pX[my_planet]-pX[curr];
+	  double dY = pY[my_planet]-pY[curr];
+	  double dZ = pZ[my_planet]-pZ[curr];
 	  // TODO: replace rsqrt macro definition with CUDA builtin rsqrt, it'll
 	  // get rid of a division which could be big
-	  float recip_dist = rsqrt(dX*dX+dY*dY+dZ*dZ);
+	  double recip_dist = sqrt(dX*dX+dY*dY+dZ*dZ);
+#ifdef VARIABLE_TS
+	  if (my_planet == 0 && curr == 1)
+	    TIME_STEP = .001 + recip_dist / 1e9;
+#endif
+	  recip_dist = 1 / recip_dist;
+	  // 1 / d
+
 	  // want to get rid of all conditionals
 	  // replace
 	  /*
 	     if (recip_dist < 100000)
-	       collisions[my_planet] = curr;
-	  */
+	     collisions[my_planet] = curr;
+	   */
 	  // we can get pretty close. We give the range that curr is in by marking
 	  // a bit flag. Let the CPU figure out which one it wants to merge
 	  // also enables us to do more than one collision
-	  collided_with |= (recip_dist > .000001) << (curr >> NOFB_BIT_SIZE);
-	  recip_dist *= masses[my_planet] * masses[curr] * GRAV_CONST;
-	  deltaX += dX * recip_dist;
-	  deltaY += dY * recip_dist;
-	  deltaZ += dZ * recip_dist;
+	  collided_with |= (recip_dist > .000000001) << (curr >> NOFB_BIT_SIZE);
+
+	  //collided_with |= (recip_dist > .000000001);
+	  double ds = recip_dist*recip_dist*recip_dist * masses[curr] * GRAV_CONST;
+	  // a = m1 * G / d^2
+	  deltaX -= dX * ds;
+	  deltaY -= dY * ds;
+	  deltaZ -= dZ * ds;
 	}
-	vX[my_planet] += deltaX;
-	vY[my_planet] += deltaY;
-	vZ[my_planet] += deltaZ;
+	vX[my_planet] += deltaX * TIME_STEP;
+	vY[my_planet] += deltaY * TIME_STEP;
+	vZ[my_planet] += deltaZ * TIME_STEP;
 	collisions[my_planet] = collided_with;
       }
 #pragma acc kernels async(1) wait(2)
-      for(curr = 0; curr < nofB; ++curr){
+      for(int curr = 0; curr < nofB; ++curr){
 	pX[curr] += vX[curr] * TIME_STEP;
 	pY[curr] += vY[curr] * TIME_STEP;
 	pZ[curr] += vZ[curr] * TIME_STEP;
       }
     }
-    int num_shiftdowns = 0;
 #pragma acc update host(collisions)
     for (int i = 0; i < nofB; ++i){
       while (collisions[i] != 0){
 #pragma acc update host(vX, vY, vZ, pX, pY, pZ)
 	int p1 = i;
 	int p2;
-	float minLength = 2e100;
+	double minLength = 2e100;
 	for (int j = 0; j < nofB; ++j){
 	  if ((j >> NOFB_BIT_SIZE) & collisions[i] == 0)
 	    continue;
 	  collisions[i] ^= 1 << (j >> NOFB_BIT_SIZE);
-	  float dX = pX[i] - pX[j];
-	  float dY = pY[i] - pY[j];
-	  float dZ = pZ[i] - pZ[j];
-	  float dist;
+	  double dX = pX[i] - pX[j];
+	  double dY = pY[i] - pY[j];
+	  double dZ = pZ[i] - pZ[j];
+	  double dist;
 	  if ((dist = sqrt(dX*dX + dY*dY + dZ*dZ)) < minLength){
 	    p2 = j;
 	    minLength = dist;
@@ -136,15 +165,13 @@ int main() {
 	  masses[j] = pX[j+1];
 	}
 	collisions[i] = 0;
-        --nofB;
+	--nofB;
       }
 #pragma acc wait(1)
 #pragma acc update device(nofB, pX, pY, pZ, vX, vY, vZ, masses)
     }
 #pragma acc wait(1)
-    // this is where you might write some display stuff
-    // for now it just prints out 
-    //
   }
-  printf("%d\n", nofB);
+#pragma acc update host(pX)
+  std::cout << nofB<< "\n";
 }
